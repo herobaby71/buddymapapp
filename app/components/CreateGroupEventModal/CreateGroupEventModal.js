b242a02d2973e4f4b570fb3e83a683a1b4cd6dc1
Admin <admin@tests-MacBook-Pro-3.local>
2018-04-21 21:22:31 -0400

    moved event stuff to the modal for it instead, textinput not working tho somehow

--- app/components/CreateGroupEventModal/CreateGroupEventModal.js
+++ app/components/CreateGroupEventModal/CreateGroupEventModal.js
@@ -2,12 +2,36 @@ import React, {Component} from 'react'
 import {TouchableOpacity, TextInput, View, Text} from 'react-native'
 import Modal from 'react-native-modal'
 
+import { Actions } from 'react-native-router-flux' //navigation
+// import * as plateSelector from '../../data/plates/selector'
+// import * as cartSelector from '../../data/cart/selector'
+
+//import styles from './styles'
+
+import DateTimePicker from 'react-native-modal-datetime-picker';
+
 class CreateGroupEventModal extends Component{
   constructor(props){
     super(props)
     this.state = {
-      modalVisible: false
+      modalVisible: false,
+      isDateTimePickerVisible: false,
+      nameOfEvent: "Enter text",
+      //descriptionOfEvent: null,
+      //locationOfEvent: null,
+      dateOfEvent: null
+      //timeOfEvent: null
     }
+  }
+
+  _showDateTimePicker = () => this.setState({ isDateTimePickerVisible: true });
+
+  _hideDateTimePicker = () => this.setState({ isDateTimePickerVisible: false });
+
+  _handleDatePicked = (date) => {
+    this._hideDateTimePicker();
+    this.setState({dateOfEvent: date});
+    console.log('the date stored is', this.state.dateOfEvent);
   }
 
   componentWillReceiveProps(newProps) {
@@ -16,12 +40,21 @@ class CreateGroupEventModal extends Component{
   }
 
   _renderModalContent = () => (
-    <View style = {this.props.containerStyle}>
-      <Text>Create Group Event Modal</Text>
-      <TouchableOpacity onPress = {this.props.hideModal}>
-        <Text>Hide Modal</Text>
-      </TouchableOpacity>
-    </View>
+      <View style = {this.props.containerStyle}>
+        <Text> NameofEvent </Text>
+        <TextInput
+          value={this.state.nameOfEvent}
+          onChangeText = { value => this.setState({nameOfEvent: value})}
+        />
+        <TouchableOpacity onPress={this._showDateTimePicker}>
+          <Text>Show DatePicker</Text>
+        </TouchableOpacity>
+        <DateTimePicker
+          isVisible={this.state.isDateTimePickerVisible}
+          onConfirm={this._handleDatePicked}
+          onCancel={this._hideDateTimePicker}
+        />
+      </View>
   );
 
   render(){
